7c4e28984573d1104b2266fa74a3dd283bc26094
Elton Stoneman <elton@sixeyed.com>
2019-07-25 06:55:56 -0700

    Add master setup scripts

---
+++ setup/ub-1804-master.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+POD_NETWORK_CIDR="10.244.0.0/16"
+SERVICE_CIDR="10.96.0.0/12"
+
+# initialise Kube
+
+kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR --service-cidr=$SERVICE_CIDR
+
+# install Flannel pod network
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
+
+wget https://raw.githubusercontent.com/sixeyed/k8s-win/master/setup/flannel/kube-flannel-hybrid.yml
+
+kubectl apply -f kube-flannel-hybrid.yml
+
+# write out config
+cat $HOME/.kube/config
---
+++ setup/ub-1804-setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DOCKER_VERSION="5:19.03.0~3-0~ubuntu-bionic"
+KUBERNETES_VERSION="1.15.1-00"
+
+# turn off swap - for the Kubelet
+swapoff -a 
+
+# install Docker 18.09 (https://docs.docker.com/install/linux/docker-ce/ubuntu/)
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -y \
+    docker-ce=$DOCKER_VERSION \
+    docker-ce-cli=$DOCKER_VERSION \
+    containerd.io
+
+# install Kubeadm etc.
+# Note - Bionic packages not available yet
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y \
+    kubelet=$KUBERNETES_VERSION \
+    kubeadm=$KUBERNETES_VERSION \
+    kubectl=$KUBERNETES_VERSION
